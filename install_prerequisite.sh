fb98ebf474a4d446e360ef5f1b88d4bf9bbd0c6d
rusynvasa <rusyn.v.i@gmail.com>
2023-06-01 12:24:21 +0300

    ansible-galaxy install gantsign.oh-my-zsh

---
+++ install_prerequisite.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+sudo apt install -y ansible
+
+ansible --version
+
+sudo apt install ssh
+sudo systemctl enable sshd
+
+# sed -i /etc/sudoers -re 's/^%sudo.*/%sudo ALL=(ALL:ALL) NOPASSWD: ALL/g'
+# sed -i /etc/sudoers -re 's/^root.*/root ALL=(ALL:ALL) NOPASSWD: ALL/g'
